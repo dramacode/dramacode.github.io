3963368793d3ad7abccdcd4f05db5059f1b075be
glorieux-f <frederic.glorieux@fictif.org>
2016-04-08 10:36:54 +0200

    typo :o(

--- index.php
+++ index.php
@@ -9,8 +9,8 @@ include('build.php');
   </head>
   <body>
       <article id="article">
-        <h1><a href="http://dramacode.github.io/">Dramacode</a>, le catatalogue, textes de théâtre en libre accès</h1>
-        <p>Ce site hébergé sur Github permet d’exposer les textes libres de la communauté “Dramacode”, des personnes intéressées par l’étude informatisée du Théâtre classique. Les fichiers sources sont en XML/TEI, et maintenus comme des projets de code sur <a href="http://dramacode.github.io/">Github</a>. Cette page est générée automatiquement à destination des chercheurs qui ont besoin d’un point d’accès rapide pour télécharger un texte dans un format ou un autre, ce n’est le site officiel d’aucun des partenaires dramacode. Retrouvez un lien sur l’édition dans son contexte éditorial dans la colonne Éditeur.</p>
+        <h1><a href="https://github.com/dramacode/">Dramacode</a>, le catalogue, textes de théâtre en libre accès</h1>
+        <p>Ce site hébergé sur Github permet d’exposer les textes libres de la communauté “Dramacode”, des personnes intéressées par l’étude informatisée du Théâtre classique. Les fichiers sources sont en XML/TEI, et maintenus comme des projets de code sur <a href="https://github.com/dramacode/">Github</a>. Cette page est générée automatiquement à destination des chercheurs qui ont besoin d’un point d’accès rapide pour télécharger un texte dans un format ou un autre, mais nous vous invitons surtout à retrouver  l’édition sur le site du partenaire, en cliquant le lien dans la colonne Éditeur.</p>
         <?php
 $base = new Dramacode('dramacode.sqlite');
 $base->table();
